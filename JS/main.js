1967bb5287274d4d2a3af592e73e2d85afc62688
patrikns <patrik.n.samuelsson@gmail.com>
2021-03-13 09:31:53 +0100

    Fixes...

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,19 @@
-document.getElementById("myForm").addEventListener("submit", placeOrder);
-
-function placeOrder () {
-    let name = document.getElementById("name").value, 
-    phone = document.getElementById("phone").value, 
-    email = document.getElementById("email").value, 
-    address = document.getElementById("address").value, 
-    city = document.getElementById("city").value;
-
-    if(!validateForm(name, phone, email, address, city)){
-        return false;
-    }
-
-    var order = {
-        name, phone, email, address, city
-    }
-
-    if(localStorage.getItem("orders") === null){
-        var orders = [];
-        orders.push(order);
-        localStorage.setItem("orders", JSON.stringify(orders));
-    }
-    else{
-        var orders = JSON.parse(localStorage.getItem("orders"));
-        orders.push(order);
-        localStorage.setItem("orders", JSON.stringify(orders));
-    }
-
-    document.getElementById("myForm").reset();
+function getItem(id) {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(resp => resp.json())
+        .then(data => addToCart(data))
+        .catch(err => console.error(err));
 }
 
-function validateForm (name, phone, email, address, city) {
-    if(!name || !phone || !email || !address || !city){
-        alert("Please fill in form!")
-        return false;
+function addToCart(item) {
+    if (localStorage.getItem("cart") === null) {
+        var cart = [];
+        cart.push(item);
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    else {
+        var cart = JSON.parse(localStorage.getItem("cart"));
+        cart.push(item);
+        localStorage.setItem("cart", JSON.stringify(cart));
     }
 }
